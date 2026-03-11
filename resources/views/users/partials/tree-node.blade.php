80d711212050c5afd2af98936f9f25d851c08afd
fathanbeje <fathanbeje@gmail.com>
2026-03-11 22:18:10 +0700

    Show spouses only on the focused tree node

--- resources/views/users/partials/tree-node.blade.php
+++ resources/views/users/partials/tree-node.blade.php
@@ -1,33 +1,21 @@
 @php
-    $hasOriginFamily = !empty($node['origin_family']) && (!empty($node['origin_family']['spouse']) || !empty($node['origin_family']['is_unmapped']));
-    $supportRowCount = $node['spouse_labels']->count() + ($hasOriginFamily ? 1 : 0);
+    $displaySpouseLabels = !empty($isRoot) ? $node['spouse_labels'] : collect();
+    $supportRowCount = $displaySpouseLabels->count();
     $entryMinHeight = max(126, 96 + ($supportRowCount * 42));
 @endphp
 
 <div class="entry {{ $node['children']->count() === 1 ? 'sole' : '' }} {{ !empty($isRoot) ? 'entry-root' : '' }}" style="min-height: {{ $entryMinHeight }}px;">
     <div class="tree-node-card">
         <span class="label">{{ link_to_route('users.tree', $node['user']->name, [$node['user']->id], ['title' => $node['user']->name.' ('.$node['user']->gender.')']) }}</span>
-        @if ($hasOriginFamily || $node['spouse_labels']->isNotEmpty())
+        @if ($displaySpouseLabels->isNotEmpty())
         <div class="tree-node-card__supporting">
-            @if ($hasOriginFamily)
-            <div class="tree-node-card__origin">
-                <span class="tree-node-card__origin-prefix">{{ trans('app.family_branch_origin') }}</span>
-                @if (!empty($node['origin_family']['spouse']))
-                    {{ $node['origin_family']['spouse']->name }}
-                @else
-                    {{ trans('app.family_branch_unmapped_short') }}
-                @endif
-            </div>
-            @endif
-            @if ($node['spouse_labels']->isNotEmpty())
             <div class="tree-node-card__spouses">
-                @foreach ($node['spouse_labels'] as $spouseLabel)
+                @foreach ($displaySpouseLabels as $spouseLabel)
                 <div class="tree-node-card__spouse">
                     {{ link_to_route('users.tree', $spouseLabel->name, [$spouseLabel->id], ['title' => $spouseLabel->name.' ('.$spouseLabel->gender.')']) }}
                 </div>
                 @endforeach
             </div>
-            @endif
         </div>
         @endif
     </div>
